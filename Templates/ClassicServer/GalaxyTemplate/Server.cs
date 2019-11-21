538071fa90250485fa810f289b8910fe04e44a38
Denis <defraswiew@gmail.com>
2019-11-22 00:39:42 +0300

    load unity

--- Templates/ClassicServer/GalaxyTemplate/Server.cs
+++ Templates/ClassicServer/GalaxyTemplate/Server.cs
@@ -8,16 +8,27 @@ using GalaxyTemplate.Connecting;
 
 namespace GalaxyTemplate
 {
-   public class Server
+    public class Server
     {
         /// <summary>
         /// Пример реализации авторизации
         /// </summary>
-        Authorization authorization;
-      
+        Authorization authorization = new Authorization();
+        /// <summary>
+        /// Конфигурация сервера
+        /// </summary>
+        Config config = new Config();
+
+        InMessages inMessages = new InMessages();
+
         public Server()
         {
-            authorization = new Authorization();
+            config.incomingMessage = inMessages; // Регистрируем обработчик входящих сообщений
+            //Задаем имя сервера
+            //Важно что бы имя сервера совпадало с именем указанным в клиенте
+            config.SERVER_NAME = "ClassicTemplate";
+            config.LISTEN_PORT = 30200; // Указываем рабочий порт
+            GalaxyCore.Start(config); // Запускаем сервер
         }
 
     }
