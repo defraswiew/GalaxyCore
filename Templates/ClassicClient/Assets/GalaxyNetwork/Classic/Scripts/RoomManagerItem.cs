a6fb0af903dfdaa50a7ce6af89c64e8340d9605b
Denis <defraswiew@gmail.com>
2019-11-23 23:23:54 +0300

    работа с комнатами

---
+++ Templates/ClassicClient/Assets/GalaxyNetwork/Classic/Scripts/RoomManagerItem.cs
@@ -0,0 +1,30 @@
+﻿using GalaxyCoreLib;
+using GalaxyTemplateCommon;
+using GalaxyTemplateCommon.Messages;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RoomManagerItem : MonoBehaviour
+{
+    public Text id;
+    public Text roomName;
+    public Text clients;
+    private int myId;
+    
+    public void SetInfo(int id, string roomName, uint clients, uint maxClients)
+    {
+        myId = id;
+        this.id.text = id.ToString();
+        this.roomName.text = roomName;
+        this.clients.text = "Онлайн: " + clients + ",  Максимум: " + maxClients;
+    }
+
+    public void ConnectRoom()
+    {
+        MessageRoomEnter message = new MessageRoomEnter();
+        message.id = myId;
+        GalaxyApi.send.SendMessageToServer((byte)CommandType.roomEnter, message, GalaxyCoreCommon.GalaxyDeliveryType.reliable);
+    }
+    
+}
---
+++ Templates/ClassicServer/GalaxyTemplateCommon/Messages/MessageRoomInfo.cs
@@ -0,0 +1,29 @@
+﻿using GalaxyCoreCommon;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalaxyTemplateCommon.Messages
+{
+    [ProtoContract]
+    public class MessageRoomInfo: BaseMessage
+    {
+        [ProtoMember(1)]
+        public List<RoomInfo> rooms { get; set; }
+    }
+
+    [ProtoContract]
+    public class RoomInfo:BaseMessage
+    {
+        [ProtoMember(1)]
+        public string name { get; set; }
+        [ProtoMember(2)]
+        public uint clients { get; set; }
+        [ProtoMember(3)]
+        public uint maxClients { get; set; }
+        [ProtoMember(4)]
+        public int id { get; set; }
+    }
+
+}
