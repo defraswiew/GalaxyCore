ad08a24127535f7e89ab495143bedbb4b6253a29
Денис <td@champer.ru>
2019-12-10 22:41:15 +0300

    Удаление объектов при дисконекте их владельца Восстановление состояния объектов для вновь вошедших игроков

---
+++ Templates/ClassicServer/GalaxyTemplateCommon/Messages/MessageWorldSync.cs
@@ -0,0 +1,18 @@
+﻿using GalaxyCoreCommon;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalaxyTemplateCommon.Messages
+{
+    /// <summary>
+    /// Сообщение синхронизации мира
+    /// </summary>
+    [ProtoContract]
+   public class MessageWorldSync:BaseMessage
+    {
+        [ProtoMember(1)]
+        public List<MessageInstantiate> netObjects { get; set; }
+    }
+}
