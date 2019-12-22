ee213b96aad72cd3fc4ca09c25667b10d88db992
Денис <td@champer.ru>
2019-12-23 00:38:20 +0300

    Upload

---
+++ Templates/SimpleMmoClient/Assets/GalaxyNetwork/Classic/Scripts/RoomManagerItem.cs
@@ -0,0 +1,31 @@
+﻿using GalaxyCoreLib;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RoomManagerItem : MonoBehaviour
+{
+    public Text id;
+    public Text roomName;
+    public Text clients;
+    private int myId;
+    
+    public void SetInfo(int id, string roomName, int clients, int maxClients)
+    {
+        myId = id;
+        this.id.text = id.ToString();
+        this.roomName.text = roomName;
+        this.clients.text = "Онлайн: " + clients + ",  Максимум: " + maxClients;
+    }
+
+    public void ConnectRoom()
+    {
+        /*
+        MessageRoomEnter message = new MessageRoomEnter();
+        message.id = myId;
+        GalaxyApi.send.SendMessageToServer((byte)CommandType.roomEnter, message, GalaxyCoreCommon.GalaxyDeliveryType.reliable);
+        */
+    }
+    
+}
---
+++ Templates/SimpleMmoClient/Assets/GalaxyNetwork/Core/Scripts/GalaxyNetworkController.cs
@@ -0,0 +1,63 @@
+﻿using GalaxyCoreLib;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GalaxyNetworkController : MonoBehaviour
+{
+    /// <summary>
+    /// Ip адрес сервера
+    /// </summary>
+    public string serverIP = "127.0.0.1";
+    /// <summary>
+    /// Порт сервера
+    /// </summary>
+    public int serverPort = 30200;
+    /// <summary>
+    /// Конфигурация клиента
+    /// </summary>
+    Config config = new Config();
+    private void Awake()
+    {
+        config.serverIp = serverIP; // задаем указаный ип
+        config.serverPort = serverPort; // задаем указанный порт
+        config.app_name = "SimpleMmoServer"; // должно соответствовать имени сервера
+        config.FrameRate = 20; // Устанавливаем сетевой фреймрейт
+        GalaxyClientCore.Initialize(config); // инициализируем сетевое ядро
+        GalaxyClientCore.unityCalls.Awake(); // прокидываем Awake
+        DontDestroyOnLoad(gameObject); // Помечаем объект как неразрушаемый при переходах между сценами        
+    }
+    private void Start()
+    {
+        GalaxyClientCore.unityCalls.Start(); // прокидываем Start
+        SceneManager.activeSceneChanged += SceneChanged;
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    private void SceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        GalaxyClientCore.unityCalls.OnSceneLoaded();
+    }
+
+    private void SceneChanged(Scene arg0, Scene arg1)
+    {
+        GalaxyClientCore.unityCalls.OnSceneChange();
+    }
+
+    private void Update()
+    {
+        GalaxyClientCore.unityCalls.Update(Time.deltaTime); // Прокидываем update
+    }
+
+
+    private void OnApplicationQuit()
+    {
+        GalaxyApi.connection.Disconnect(); //Посылаем команду дисконекта при выходе из приложения
+    }
+
+    private void OnDisable()
+    {
+        GalaxyApi.connection.Disconnect();//Посылаем команду дисконекта если go был выключен
+    }
+}
