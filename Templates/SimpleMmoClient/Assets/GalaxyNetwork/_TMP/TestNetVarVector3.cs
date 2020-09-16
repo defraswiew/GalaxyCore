fa1b1f89ff240783551ff26065bff023460d909e
Денис <defraswiew@gmail.com>
2020-09-16 14:02:19 +0300

    OnDisconnected(); Tools.GetUnixTime(); galaxyVars.ForceSync(); bool autoSyncInstance;

---
+++ Templates/SimpleMmoClient/Assets/GalaxyNetwork/_TMP/TestNetVarVector3.cs
@@ -0,0 +1,10 @@
+﻿using GalaxyCoreCommon;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNetVarVector3 : MonoBehaviour
+{
+    [GalaxyVar(12)]
+    public GalaxyVector3 galaxyVector;
+}
